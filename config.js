cb8e0c952a33c0d3eab67402b7f44ff648b3f2c4
vimukthioshada <77786139+vimukthioshada@users.noreply.github.com>
2025-11-13 20:28:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 ALIVE_IMG : process.env.ALIVE_IMG || "https://pomf2.lain.la/f/id9p5zqm.jpg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-    SESSION_ID: process.env.SESSION_ID || "3IIDzKCL#Lr1PgfeCejP6kyP6IIbegkI3YAPDVp4qdGwEpolMcRg",
+    SESSION_ID: process.env.SESSION_ID || "",
     OWNER_NUMBER: process.env.OWNER_NUMBER || "94755773910",
     AUTO_VIEW_STATUS: process.env.AUTO_VIEW_STATUS || "true",
     MODE: process.env.MODE || "public",
@@ -17,7 +17,7 @@ AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REACT: process.env.AUTO_REACT || "true",
     OWNER_NAME: process.env.OWNER_NAME || "wolf",
     BOT_NAME: process.env.BOT_NAME || "wolf",
-    MONGODB: process.env.MONGODB || "mongodb+srv://redwolf:redwolf@redwolf.rjjjr.mongodb.net/?",
+    MONGODB: process.env.MONGODB || "mongodb+srv://oshadavimukthi555:<db_password>@filmxyz.xptc9.mongodb.net/?",
    CAPTION: process.env.CAPTION || "Made By Wolf",
     FOOTER: process.env.FOOTER || "Wolf",
     JIDS: ["120363045197379067@g.us"], // MOVIE GROUP WALA JIDs
